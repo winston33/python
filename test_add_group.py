7f9b51bc1419a2dac26463692425c93a99344baa
Winston <Winston@DV7>
2017-01-08 13:49:45 +0300

    readme improvement

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from group import Group
 import unittest
 
 
@@ -16,35 +17,50 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         binary = FirefoxBinary(r'c:\Program Files (x86)\Mozilla Firefox\firefox.exe')
         self.wd = WebDriver(firefox_binary=binary)
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(100)
     
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost:8070/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("Группы").click()
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group(name="study", header="good", footer="Not friends"))
+        self.open_group_page(wd)
+        self.log_out(wd)
+        self.assertTrue(success)
+
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("vxcv")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("xcv")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("cbcvb")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
+
+    def log_out(self, wd):
         wd.find_element_by_link_text("Выйти").click()
-        self.assertTrue(success)
-    
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("Группы").click()
+
+    def login(self, wd, name, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(name)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost:8070/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
